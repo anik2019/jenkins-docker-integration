9a841baecfa82be5994fe7cf3157cf693cc57bd5
Anik Nandy <anik.nandy@ibm.com>
2022-10-15 12:22:33 +0530

    first commit

---
+++ main/java/com/microservices/springboot/docker/Application.java
@@ -0,0 +1,20 @@
+package com.microservices.springboot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello...This is from Springboot Application...";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
